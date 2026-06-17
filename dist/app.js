61f18bcb17622e2b71e924e802aa4190abf580d5
amcoddieng <dieng0097@gmail.com>
2026-06-17 16:38:36 +0000

    autorisation de netlify

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,16 @@ import notificationRoutes from './route/notificationRoutes.js';
 import messageRoutes from './route/messageRoutes.js';
 import dossierUniversiteRoutes from './route/dossierUniversiteRoutes.js';
 const app = express();
+
+// autoriser "https://capadmis.netlify.app",
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://capadmis.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 app.use(json());
 app.use('/api/auth', authRoutes);
 app.use('/api/personnel', personnelRoutes);
