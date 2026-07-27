f7cab77d8b21957842d98bb443fa3312b393612a
amcoddieng <dieng0097@gmail.com>
2026-07-27 11:43:10 +0000

    modification du footer

--- dist/route/dossierUniversiteRoutes.js
+++ dist/route/dossierUniversiteRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { createDossierUniversite, getAllDossiersUniversite, getDossierUniversiteById, updateDossierUniversite, deleteDossierUniversite, getDossiersUniversiteByDossier, } from '../controllers/dossierUniversiteController.js';
 import { verifyPersonnelToken } from '../middleware/personnelMiddleware.js';
-import { verifyEtudiantOrPersonnelToken } from '../middleware/mixedAuthMiddleware.js';
+import { verifyAnyToken } from '../middleware/hybridMiddleware.js';
 const router = Router();
-router.get('/dossier/:code_dossier', verifyEtudiantOrPersonnelToken, getDossiersUniversiteByDossier);
+router.get('/dossier/:code_dossier', verifyAnyToken, getDossiersUniversiteByDossier);
 router.post('/', verifyPersonnelToken, createDossierUniversite);
 router.get('/', verifyPersonnelToken, getAllDossiersUniversite);
 router.get('/:id', verifyPersonnelToken, getDossierUniversiteById);
